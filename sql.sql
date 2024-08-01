e9ffff2b468d8a5a437a585433c0678e6f96eb11
Oleh <314koval.ok@gmail.com>
2024-08-01 12:20:06 +0300

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,8 +1,10 @@
+
+/* Тільки по діючим на поточний момент договорам*/
 SELECT lee.registration_area, lee.edrpou, lee.kwd_name, 
-       contract_number, contract_start_date, contract_end_date
+       m.contract_number, m.contract_start_date, m.contract_end_date
   FROM analytics.dwh_legal_entities_edrpou_view AS lee
        JOIN analytics.dwh_contract_master_smarttender_view m ON m.edrpou = lee.edrpou
        JOIN analytics.dwh_smd_contract_specifications_view as sp ON sp.contract_id = m.id
- WHERE specification_id = '42'
+ WHERE sp.specification_id = '42' -- номер пакету
    AND lee.status = 'ACTIVE'
- ORDER BY lee.registration_area, lee.edrpou 
+ ORDER BY lee.registration_area, lee.edrpou   
